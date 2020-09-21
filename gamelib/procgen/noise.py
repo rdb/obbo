677ccfa0091879bd70d55a69bc7798642e4d4034
tcdude <tc@tizilogic.com>
2020-09-21 02:38:51 +0200

    Map to sphere instead of cylinder

--- gamelib/procgen/noise.py
+++ gamelib/procgen/noise.py
@@ -12,13 +12,14 @@ def asteroid_noise(xy:int, z:int, radius=None, seed=None):  # pylint: disable=in
     for each component (e.g. heading, pitch, radius, color).
     """
     twopi = np.linspace(-np.pi, np.pi, xy, endpoint=False)
+    pitch = np.linspace(-np.pi / 2, np.pi / 2, z)
     radius = radius or np.sqrt(((xy / 2) ** 2) * 2)
-    step_size = 2 * np.pi * radius / xy
     x_mesh = np.cos(twopi) * radius
     y_mesh = np.sin(twopi) * radius
-    z_mesh = np.linspace(0, z * step_size, z)
+    z_val = np.sin(pitch) * radius
+    z_cos = np.cos(pitch)
     points = 4 * z * xy
-    coords = np.empty((4, points), np.float32)
+    coords = np.empty((3, points), np.float32)
     z_half = (z - 1) // 2
     for dim in range(4):
         dim_off = dim * z
@@ -27,17 +28,16 @@ def asteroid_noise(xy:int, z:int, radius=None, seed=None):  # pylint: disable=in
             z_factor = z_factor * 0.5 + 0.5
             z_off = zid * xy + dim_off
             rad_off = dim * radius * 3
-            coords[0, z_off:z_off + xy] = x_mesh + rad_off
-            coords[1, z_off:z_off + xy] = y_mesh + rad_off
-            coords[2, z_off:z_off + xy] = z_mesh[zid]
-            coords[3, z_off:z_off + xy] = z_factor
+            coords[0, z_off:z_off + xy] = x_mesh * z_cos[zid] + rad_off
+            coords[1, z_off:z_off + xy] = y_mesh * z_cos[zid] + rad_off
+            coords[2, z_off:z_off + xy] = z_val[zid]
     arr = np.empty((points,), np.float32)
 
     seed = seed or random.randrange(2 ** 31)
     osn = OpenSimplex()
     try:
         for i in range(points):
-            arr[i] = osn.noise3d(coords[0, i], coords[1, i], coords[2, i]) * coords[3, i]
+            arr[i] = osn.noise3d(coords[0, i], coords[1, i], coords[2, i])
     except (ValueError, OverflowError):
         return asteroid_noise(xy, z, radius, random.randrange(2 ** 31))
 
