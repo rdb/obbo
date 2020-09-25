94292100b548cb7d7cdb37d45a77a4a02f8c31f3
Mitchell Stokes <mogurijin@gmail.com>
2020-09-25 09:58:05 -0700

    setuptools: Add sdist_built command
    This creates an sdist with a pre-built .built_assets. This means Blender
    is not required to run from source with this sdist.
    
    Also fix up gathering files for a regular sdist.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
+import tarfile
+import tempfile
+
 from setuptools import setup
+from setuptools.command import sdist
 
 import pman.build_apps
 
@@ -6,8 +11,51 @@ CONFIG = pman.get_config()
 
 APP_NAME = CONFIG['general']['name']
 
+class SDistBuilt(sdist.sdist):
+    def run(self):
+        pman.core.build()
+        with open('MANIFEST.in') as mfile:
+            manifest = mfile.read()
+        prev = manifest
+        manifest +='\ngraft .built_assets'
+
+        with open('MANIFEST.in', 'w') as mfile:
+            mfile.write(manifest)
+
+        super().run()
+
+        with open('MANIFEST.in', 'w') as mfile:
+            mfile.write(prev)
+
+    # No way to change base_name from sub-class, so just copy/edit from base class
+    def make_distribution(self):
+        from distutils import dir_util
+        # Don't warn about missing meta-data here -- should be (and is!)
+        # done elsewhere.
+        base_dir = self.distribution.get_fullname()+'-built'
+        base_name = os.path.join(self.dist_dir, base_dir)
+
+        self.make_release_tree(base_dir, self.filelist.files)
+        archive_files = []              # remember names of files we create
+        # tar archive must be created last to avoid overwrite and remove
+        if 'tar' in self.formats:
+            self.formats.append(self.formats.pop(self.formats.index('tar')))
+
+        for fmt in self.formats:
+            file = self.make_archive(base_name, fmt, base_dir=base_dir,
+                                     owner=self.owner, group=self.group)
+            archive_files.append(file)
+            self.distribution.dist_files.append(('sdist', '', file))
+
+        self.archive_files = archive_files
+
+        if not self.keep_temp:
+            dir_util.remove_tree(base_dir, dry_run=self.dry_run)
+
+
 setup(
     name=APP_NAME,
+    packages=['gamelib'],
     setup_requires=[
         'pytest-runner',
     ],
@@ -23,6 +71,7 @@ setup(
     },
     cmdclass={
         'build_apps': pman.build_apps.BuildApps,
+        'sdist_built': SDistBuilt,
     },
     options={
         'build_apps': {
