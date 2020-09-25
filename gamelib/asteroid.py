ee1b204e8586b5753a7acb7b9634a541f1252b9e
Mitchell Stokes <mogurijin@gmail.com>
2020-09-24 20:54:51 -0700

    asteroid: Remove two unnecessary NodePath instances

--- gamelib/asteroid.py
+++ gamelib/asteroid.py
@@ -3,7 +3,6 @@ import random
 from panda3d import core
 from direct.interval.LerpInterval import *
 
-from .planet import PlanetObject
 from .procgen import asteroid
 
 
@@ -12,19 +11,18 @@ MAX_B = 0.7
 SCALE_DURATION = 0.3
 
 
-class Asteroid(PlanetObject):
+class Asteroid:
     def __init__(self, planet, universe):
-        super().__init__(planet)
         self.universe = universe
 
+        self.root = planet.root.attach_new_node("root")
         bounds = core.Vec3(*(random.uniform(MIN_B, MAX_B) for _ in range(3)))
         radius = max(bounds)
         color1 = core.Vec3(random.uniform(0.05, 0.25))
         color2 = color1 * random.uniform(0.1, 0.5)
         node = asteroid.generate(bounds, color1, color2, random.uniform(5.2, 9.5))
-        baserot = self.root.attach_new_node('Base Rotation')
-        baserot.set_hpr(random.randrange(360), random.randrange(-90, 90), 0)
-        self.rotation = baserot.attach_new_node('Rotation')
+        self.root.set_hpr(random.randrange(360), random.randrange(-90, 90), 0)
+        self.rotation = self.root.attach_new_node('Rotation')
         self.rotation.set_pos(0, 0, -1)
         self.asteroid = self.rotation.attach_new_node(node)
         self.asteroid.set_pos(
@@ -53,7 +51,7 @@ class Asteroid(PlanetObject):
 
     def destroy(self):
         self.asteroid.remove_node()
-        super().destroy()
+        self.root.remove_node()
         self.universe.asteroids.pop(self.universe.asteroids.index(self))
 
     def stop(self):
